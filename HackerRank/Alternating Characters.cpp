2cf835779b289d79336a11cc168c790213baa817
BenzhaminKim <dudgnl37@gmail.com>
2021-02-15 04:26:25 +0900

    easy

---
+++ HackerRank/Alternating Characters.cpp
@@ -0,0 +1,39 @@
+//https://www.hackerrank.com/challenges/alternating-characters/problem
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the alternatingCharacters function below.
+int alternatingCharacters(string s) {
+    int ans = 0;
+    
+    for(int i=1; i<(int)s.length(); ++i){
+        if(s[i-1] == s[i]){
+            ans++;
+        }
+    }
+    return ans;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = alternatingCharacters(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
